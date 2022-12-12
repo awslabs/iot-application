4b62757094d12ad83e1a798755e8373d4d534a44
Tracy French <tracfren@amazon.com>
2022-12-12 14:22:01 -0700

    initial setup of vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "happy-dom",
+    globals: true,
+    setupFiles: "./tests/setup.js",
+  },
+});
