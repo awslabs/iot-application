bcf8a7df3ceea7ceb5f36890169e5c3fa618bcb8
Tracy French <tracfren@amazon.com>
2022-12-13 10:23:30 -0700

    add cloudscape

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+import Button from "@cloudscape-design/components/button";
+import Container from "@cloudscape-design/components/container";
+import Header from "@cloudscape-design/components/header";
+import Input from "@cloudscape-design/components/input";
+import SpaceBetween from "@cloudscape-design/components/space-between";
+
 function App() {
+  const [value, setValue] = useState("");
+
   return (
-    <div>
-      <h1>IoT Application</h1>
-      <button>Enter</button>
-    </div>
+    <SpaceBetween size="m">
+      <Header variant="h1">Centurion</Header>
+
+      <Container>
+        <SpaceBetween size="s">
+          <span>Start editing to see some magic happen</span>
+          <Input
+            value={value}
+            onChange={(event) => setValue(event.detail.value)}
+          ></Input>
+          <Button variant="primary">Click me</Button>
+        </SpaceBetween>
+      </Container>
+    </SpaceBetween>
   );
 }
 
