9775e08c026dcb1df790c94c2a3801430e34faa7
Tracy French <tracfren@amazon.com>
2022-12-12 20:25:39 -0700

    add eslint and prettier

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootEl = document.getElementById("root");
+
+if (rootEl != null) {
+  ReactDOM.createRoot(rootEl).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
