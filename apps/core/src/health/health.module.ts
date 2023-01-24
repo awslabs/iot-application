c3b2ede2a285051f6a9f86dbcd96e9543e9c54db
Tracy French <tracfren@amazon.com>
2023-01-24 15:36:04 -0700

    add initial health module

---
+++ apps/core/src/health/health.module.ts
@@ -0,0 +1,12 @@
+import { HttpModule } from "@nestjs/axios";
+import { Module } from "@nestjs/common";
+import { TerminusModule } from "@nestjs/terminus";
+
+import { HealthController } from "./health.controller";
+
+/** Application health status */
+@Module({
+  imports: [TerminusModule, HttpModule],
+  controllers: [HealthController],
+})
+export class HealthModule {}
