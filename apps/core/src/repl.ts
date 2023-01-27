4efaf2a1738b5e5023492892877182b12376c260
Tracy French <tracfren@amazon.com>
2023-01-27 16:22:18 -0700

    setup core repl

---
+++ apps/core/src/repl.ts
@@ -0,0 +1,30 @@
+import { repl } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+/**
+ * Main() for Core REPL
+ *
+ * @regards
+ *
+ * With the IoT Application Core REPL, the internals of Core are directly
+ * accessible.
+ *
+ * To get started, run `pnpm repl` and the Core REPL will boot.
+ *
+ * @see {@link https://docs.nestjs.com/recipes/repl}
+ * @see {@link https://docs.nestjs.com/recipes/repl#watch-mode}
+ *
+ * @internal
+ */
+async function bootstrap() {
+  const replServer = await repl(AppModule);
+
+  // enables persistence of REPL commands
+  replServer.setupHistory(".nestjs_repl_history", (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+bootstrap();
