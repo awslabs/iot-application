c4d98afb18d922ada29b1b1b2cb767387849896a
Jimmy Cheung <50635800+chejimmy@users.noreply.github.com>
2023-02-02 16:16:46 -0800

    feat: added Cognito user sign in to client (#26)

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+
 import App from "./App";
+
+import '@aws-amplify/ui-react/styles.css';
 import "@cloudscape-design/global-styles/index.css";
 
 const rootEl = document.getElementById("root");
 
+let AppEl = App;
+
+// awsResources is populated by aws-resources.js
+const awsResources = (global as any).awsResources;
+Amplify.configure({
+  ...awsResources,
+  // Overrides go here
+});
+AppEl = withAuthenticator(App, {
+  hideSignUp: true,
+});
+
 if (rootEl != null) {
   ReactDOM.createRoot(rootEl).render(
     <React.StrictMode>
-      <App />
+      <AppEl />
     </React.StrictMode>,
   );
 }
