4e47a5e4fe1968a48f13cb0e8e012a8751fee2af
Norbert Nader <Norbert.Nader@gmail.com>
2023-02-10 10:00:53 -0800

    feat: add @iot-ap-kit/dashboard to dashboard page (#34)

--- apps/client/src/index.tsx
+++ apps/client/src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import '@aws-amplify/ui-react/styles.css';
 import '@cloudscape-design/global-styles/index.css';
 
 import router from './routes/router';
-import './styles/index.css';
 
 const rootEl = document.getElementById('root');
 
