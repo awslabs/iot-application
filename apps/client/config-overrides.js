b926474446c1840426ec8d22c13a4814f630e046
Jimmy Cheung <50635800+chejimmy@users.noreply.github.com>
2023-02-21 11:01:17 -0800

    feat: added client endpoint to vend AWS credentials under local development (#51)

---
+++ apps/client/config-overrides.js
@@ -0,0 +1,29 @@
+const { STSClient, GetSessionTokenCommand } = require("@aws-sdk/client-sts");
+
+module.exports = {
+  devServer: function(configFunction) {
+    return function(proxy, allowedHost) {
+      const config = configFunction(proxy, allowedHost);
+
+      // Overrides the original `devServer.onBeforeSetupMiddleware` to add a custom AWS Credentials vending handler
+      const origOnBeforeSetupMiddleware = config.onBeforeSetupMiddleware;
+
+      config.onBeforeSetupMiddleware = function (devServer) {
+        if (!devServer) {
+          throw new Error('webpack-dev-server is not defined');
+        }
+
+        origOnBeforeSetupMiddleware(devServer);
+  
+        devServer.app.get('/credentials.json', async (req, res) => {
+          const stsClient = new STSClient({});
+          const token = await stsClient.send(new GetSessionTokenCommand({}));
+          
+          res.json(token.Credentials);
+        });
+      };
+
+      return config;
+    };
+  },
+};
