d9bb2ec9001a7d80c402f9fcd4a970a63f5037d2
Jimmy Cheung <50635800+chejimmy@users.noreply.github.com>
2023-02-21 21:12:57 -0800

    fix: run core:test w/o dynamo db

--- apps/core/jest.config.ts
+++ apps/core/jest.config.ts
@@ -2,12 +2,8 @@ import type { Config } from 'jest';
 
 import baseConfig from 'jest-config/base';
 
-const jestDynamodb = require('@shelf/jest-dynamodb/jest-preset');
-
 const config: Config = {
   ...baseConfig,
-  //https://jestjs.io/docs/dynamodb
-  ...jestDynamodb,
   collectCoverageFrom: [
     '**/src/**/*.{js,ts}',
     '!./src/main.ts',
