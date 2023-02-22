153148445b7361cddf565e12795e7b996bf3511f
Jimmy Cheung <50635800+chejimmy@users.noreply.github.com>
2023-02-21 22:44:12 -0800

    fix: no resource table creation

--- apps/core/src/lifecycle-hooks/dynamodb-local-setup.ts
+++ apps/core/src/lifecycle-hooks/dynamodb-local-setup.ts
@@ -35,9 +35,8 @@ export class DynamoDbLocalSetupService implements OnApplicationBootstrap {
       this.logger.log('Launching DynamoDB local instance');
       await ddbLocalLaunch(this.dbConfig.port);
       this.logger.log('DynamoDB local instance launched');
+      await this.createApiResourceTable();
     }
-
-    await this.createApiResourceTable();
   }
 
   private async createApiResourceTable() {
