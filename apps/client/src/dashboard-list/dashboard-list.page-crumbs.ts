a04846dd07b8754fbd320745fce47956efd65d2e
tracy-french <tracyfrenchengineer@gmail.com>
2023-03-02 12:10:51 -0700

    fix: swap page crumbs

--- apps/client/src/dashboard-list/dashboard-list.page-crumbs.ts
+++ apps/client/src/dashboard-list/dashboard-list.page-crumbs.ts
@@ -2,6 +2,4 @@ import type { PageCrumb } from '../breadcrumbs/page-crumb.type';
 
 export const DASHBOARD_LIST_PAGE_CRUMBS: PageCrumb[] = [
   { text: 'Dashboards', href: '/dashboards' },
-  // TODO: Replace with real dashboard name once resource is being loaded
-  { text: 'Dashboard Name', href: 'no-op' }, // no link needed for deepest resource
 ];
