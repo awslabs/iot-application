5b00520825dfdc33e9d2816f6587283278bc8282
tracy-french <tracyfrenchengineer@gmail.com>
2023-03-27 12:59:58 -0600

    fix: increase playwright timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 10000,
+  timeout: 20000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
