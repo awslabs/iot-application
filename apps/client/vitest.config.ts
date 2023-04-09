5a8d01f37774bb3ac79846d32ae96d095ad77ae9
tracy-french <tracyfrenchengineer@gmail.com>
2023-04-08 19:57:25 -0600

    chore: add test coverage config on client

--- apps/client/vitest.config.ts
+++ apps/client/vitest.config.ts
@@ -1,4 +1,8 @@
-import { defineConfig } from 'vitest/config';
+import {
+  defineConfig,
+  defaultExclude,
+  coverageConfigDefaults,
+} from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -13,9 +17,40 @@ export default defineConfig({
     alias: {
       '~': '/src',
     },
+    coverage: {
+      all: true,
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        'config-overrides.js',
+        'html/**/*',
+        'lang/**/*',
+        'public/**/*',
+        'src/services/generated/**/*',
+        'src/types/**/*',
+      ],
+      reporter: ['text', 'html'],
+      /**
+       * Application was initially tested with Playwright, covering core
+       * client functionality. We now plan to backfill unit tests and reach
+       * the coverage levels defined in the watermarks below.
+       *
+       * Please increase the coverage levels as you add tests.
+       */
+      statements: 22,
+      branches: 53,
+      functions: 23,
+      lines: 22,
+      watermarks: {
+        statements: [80, 95],
+        branches: [80, 95],
+        functions: [80, 95],
+        lines: [80, 95],
+      },
+    },
     environment: 'jsdom',
     globals: true,
     includeSource: ['src/**/*.{js,ts}'],
+    reporters: ['verbose', 'html'],
     setupFiles: './src/test/setup.ts',
   },
   define: {
