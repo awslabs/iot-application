57d5f9bedf94c85e462e0eeabfe1985854fb93d3
tracy-french <tracyfrenchengineer@gmail.com>
2023-04-11 21:05:55 -0600

    feat: dashboard name in breadcrumbs

--- apps/client/src/routes/dashboards/dashboard/dashboard-route.tsx
+++ apps/client/src/routes/dashboards/dashboard/dashboard-route.tsx
@@ -3,7 +3,13 @@ import { DashboardErrorBoundary } from './components/dashboard-error-boundary';
 import { DASHBOARD_PAGE_FORMAT } from '~/constants/format';
 import { DASHBOARD_PATH } from '~/constants';
 
-import type { RouteObject } from 'react-router-dom';
+import { queryClient } from '~/data/query-client';
+
+import { type RouteObject } from 'react-router-dom';
+import { createDashboardQuery } from '~/data/dashboards';
+import { Auth } from 'aws-amplify';
+import invariant from 'tiny-invariant';
+import { Dashboard } from '~/services';
 
 export const dashboardRoute = {
   path: DASHBOARD_PATH,
@@ -12,12 +18,22 @@ export const dashboardRoute = {
       <DashboardPage />
     </DashboardErrorBoundary>
   ),
+  loader: async ({ params }) => {
+    invariant(params.dashboardId, 'Expected dashboardId is to be defined');
+    /**
+     * The loader is called before the element is rendered. This means we need
+     * to wait for authentication state before we can fetch the dashboard.
+     */
+    await Auth.currentAuthenticatedUser();
+    return queryClient.fetchQuery(createDashboardQuery(params.dashboardId));
+  },
   handle: {
-    crumb: () => ({
-      // TODO: replace with real dashboard name
-      text: 'Dashboard',
-      href: '',
-    }),
+    crumb: (dashboard: Dashboard) => {
+      return {
+        text: dashboard.name,
+        href: '',
+      };
+    },
     fullWidth: true,
     format: DASHBOARD_PAGE_FORMAT,
   },
