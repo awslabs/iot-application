a86519dd3054d1ecb2a26439fbd90e7c13746935
Tracy French <67283114+tracy-french@users.noreply.github.com>
2023-04-26 17:07:48 -0600

    feat: cache dashboard requests

--- apps/core/src/app.module.ts
+++ apps/core/src/app.module.ts
@@ -1,13 +1,18 @@
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { ThrottlerModule } from '@nestjs/throttler';
+
 import { databaseConfig } from './config/database.config';
 import { DashboardsModule } from './dashboards/dashboards.module';
 import { HealthModule } from './health/health.module';
 import { DynamoDbLocalSetupService } from './lifecycle-hooks/dynamodb-local-setup';
-import { APP_GUARD } from '@nestjs/core';
 import { CognitoJwtAuthGuard } from './auth/cognito-jwt-auth.guard';
 import { authConfig } from './config/auth.config';
-import { ThrottlerModule } from '@nestjs/throttler';
+
+const SECOND_IN_MS = 1000;
+const MINUTE_IN_MS = 60 * SECOND_IN_MS;
 
 @Module({
   imports: [
@@ -15,6 +20,11 @@ import { ThrottlerModule } from '@nestjs/throttler';
     ConfigModule.forRoot({ load: [authConfig], isGlobal: true }),
     DashboardsModule,
     HealthModule,
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 10 * MINUTE_IN_MS,
+      max: 100, // max number of items in cache
+    }),
     ThrottlerModule.forRoot({ ttl: 10, limit: 100 }),
   ],
   providers: [
@@ -22,6 +32,10 @@ import { ThrottlerModule } from '@nestjs/throttler';
     {
       provide: APP_GUARD,
       useClass: CognitoJwtAuthGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
     },
   ],
 })
