9eaf3298dfd658c91918f51e8cd0031549c2b226
Tracy French <67283114+tracy-french@users.noreply.github.com>
2023-04-28 13:43:44 -0600

    chore: integrate cloudscape

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
+  transpilePackages: ['@cloudscape-design'],
 };
 
 module.exports = nextConfig;
--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Button from '@cloudscape-design/components/button';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       </Head>
       <main>
         <h1>IoT Application</h1>
+        <Button>test</Button>
       </main>
     </>
   );
