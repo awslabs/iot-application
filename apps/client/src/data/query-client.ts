482fb505b829b74d7586e7dd2ab500b6506d7c47
Tracy French <67283114+tracy-french@users.noreply.github.com>
2023-05-09 10:20:56 -0600

    feat: sign out unauthenticated users

--- apps/client/src/data/query-client.ts
+++ apps/client/src/data/query-client.ts
@@ -1,6 +1,10 @@
 import { QueryCache, QueryClient } from '@tanstack/react-query';
+import { Auth } from 'aws-amplify';
+import invariant from 'tiny-invariant';
 
 import { MINUTE_IN_MS } from '~/constants/time';
+import { isApiError } from '~/helpers/predicates/is-api-error';
+import { ApiError } from '~/services';
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -8,5 +12,19 @@ export const queryClient = new QueryClient({
       staleTime: MINUTE_IN_MS * 10,
     },
   },
-  queryCache: new QueryCache({}),
+  queryCache: new QueryCache({
+    // global error handler
+    onError: (error) => {
+      // not possible to be other errors using service client
+      invariant(isApiError(error), 'Expected error to be an API error');
+      signOutUnauthenticatedUser(error);
+    },
+  }),
 });
+
+export function signOutUnauthenticatedUser(error: ApiError) {
+  if (error.status === 401) {
+    // TODO: create friendly sign out experience
+    void Auth.signOut();
+  }
+}
