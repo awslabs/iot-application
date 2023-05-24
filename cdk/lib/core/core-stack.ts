0c4db9ae37561d499058173c887a000a282b788a
Jimmy Cheung <50635800+chejimmy@users.noreply.github.com>
2023-05-23 22:11:48 -0700

    chore: IP rate limit WAF rule for Core

--- cdk/lib/core/core-stack.ts
+++ cdk/lib/core/core-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CoreService, CoreServiceProps } from './core-service';
+import { CoreServiceWebACL } from './core-service-web-acl';
 
 export interface CoreStackProps extends StackProps {
   readonly coreServiceProps: CoreServiceProps;
@@ -13,5 +14,11 @@ export class CoreStack extends Stack {
     super(scope, id, props);
 
     this.coreService = new CoreService(this, 'Service', props.coreServiceProps);
+
+    const {
+      service: { attrServiceArn: coreServiceArn },
+    } = this.coreService;
+    const coreServiceWebACL = new CoreServiceWebACL(this, 'WebACL');
+    coreServiceWebACL.associate(coreServiceArn);
   }
 }
