01f66390e2538fdf1559dcfa0404ec84f470ac9a
Chandru_HM <chandrashekhara.n@happiestminds.com>
2023-10-19 19:49:15 +0530

    feat: updated wcag compliance for dashboard pagination buttons #1517

--- apps/client/src/routes/dashboards/dashboards-index/dashboards-index-page.tsx
+++ apps/client/src/routes/dashboards/dashboards-index/dashboards-index-page.tsx
@@ -97,6 +97,16 @@ export function DashboardsIndexPage() {
     sorting: {},
   });
 
+  const paginationPropsWithAriaLabels = {
+    ...paginationProps,
+    ariaLabels: {
+      nextPageLabel: 'Next page',
+      paginationLabel: 'Dashboard Table pagination',
+      previousPageLabel: 'Previous page',
+      pageLabel: (pageNumber: number) => `Page ${pageNumber}`,
+    },
+  };
+
   const { selectedItems = [] } = collectionProps;
 
   const { navigate } = useApplication();
@@ -193,7 +203,7 @@ export function DashboardsIndexPage() {
             });
           }}
           onEditCancel={() => resetForm()}
-          pagination={<Pagination {...paginationProps} />}
+          pagination={<Pagination {...paginationPropsWithAriaLabels} />}
           header={
             <Header
               variant="h1"
