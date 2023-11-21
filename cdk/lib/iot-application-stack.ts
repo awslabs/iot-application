a447b0eda617de5a98e6571bebf30c1a9549d573
Jimmy Cheung <50635800+chejimmy@users.noreply.github.com>
2023-11-21 12:53:18 -0800

    Revert "Revert "fix: use unique id that wont change in domain prefix name""
    This reverts commit 66b93ada81afff0187c4f74324a22798599078ab.

--- cdk/lib/iot-application-stack.ts
+++ cdk/lib/iot-application-stack.ts
@@ -24,6 +24,7 @@ export class IotApplicationStack extends Stack {
       userPool: { userPoolId },
       userPoolClient: { userPoolClientId },
       identityPool: { ref: identityPoolId },
+      domain: { domainName },
     } = new AuthStack(this, 'Auth', {
       ...props,
       applicationName: id,
@@ -47,6 +48,7 @@ export class IotApplicationStack extends Stack {
         identityPoolId: identityPoolId,
         userPoolClientId: userPoolClientId,
         userPoolId: userPoolId,
+        domainName: domainName,
       },
     });
 
