b3bca2e72ca34715b33de58240dcac7a5eeaca3f
Dylan Trachsel <dpportet@amazon.com>
2023-12-14 14:23:17 -0800

    fix: upgrade build from cra to vite

--- apps/client/src/register-service-worker.ts
+++ apps/client/src/register-service-worker.ts
@@ -1,5 +1,5 @@
 export function registerServiceWorker() {
-  const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+  const swUrl = `/service-worker.js`;
 
   navigator.serviceWorker.register(swUrl).catch((error) => {
     console.error('Error during service worker registration:', error);
---
+++ apps/client/vite.config.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dynamicImport from 'vite-plugin-dynamic-import';
+import commonjs from 'vite-plugin-commonjs';
+import path from 'path';
+
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: 'build',
+    },
+    plugins: [
+      /*
+      * This plugin is required to resolve the charts-core module as they are dynamically imported
+      * when the library is loaded.
+      * See the dynamic loading code here: https://github.com/ionic-team/stencil/blob/main/src/client/client-load-module.ts#L32
+      * Unfortunately, we are using an older version of the stencil library which doesn't use the syntax vite supports to
+      * detect the dynamic imports automatically, so we need to use this plugin to explicitly tell vite to include the
+      * charts-core module otherwise their code will not be included in the build.
+      */
+      dynamicImport({
+        filter(id) {
+          // `node_modules` is exclude by default, so we need to include it explicitly
+          // https://github.com/vite-plugin/vite-plugin-dynamic-import/blob/v1.3.0/src/index.ts#L133-L135
+          if (id.includes('/node_modules/@iot-app-kit/charts-core')) {
+            console.log('dynamically import entry point', id);
+            return true;
+          }
+        },
+        onFiles(files) {
+          console.log('dynamically import files', files);
+        }
+      }),
+      // required to support the commonjs require in dev builds
+      commonjs(),
+      react(
+      {
+        babel: {
+          babelrc: true,
+        }
+      }
+    )],
+    server: {
+      port: 3001,
+      host: true
+    },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, 'src'),
+      }
+    }
+  };
+});
