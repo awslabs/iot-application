bf4a8df9593dd6d43cc6245902d7aed78acab6ec
Dylan Trachsel <dpportet@amazon.com>
2024-02-09 11:24:59 -0800

    feat: allow customer to choose between SSO or Cognito for auth mode

---
+++ cdk/lib/auth/sso-auth-stack.ts
@@ -0,0 +1,22 @@
+import { UserPoolDomain } from 'aws-cdk-lib/aws-cognito';
+import { Construct } from 'constructs';
+import { AuthStackProps, AuthStack } from './auth-stack';
+
+export class SsoAuthStack extends AuthStack {
+  readonly domain: UserPoolDomain;
+
+  constructor(scope: Construct, id: string, props: AuthStackProps) {
+    super(scope, id, props);
+
+    this.domain = this.userPool.addDomain('Domain', {
+      cognitoDomain: {
+        // Use the unique id for this cdk construct for naming
+        domainPrefix: `sitewise-${this.node.addr.substring(0, 6)}`,
+      },
+    });
+
+    this.domain.signInUrl(this.userPoolClient, {
+      redirectUri: 'https://<your-url>.awsapprunner.com',
+    });
+  }
+}
