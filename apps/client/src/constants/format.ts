137b9bb08c5cda0915fbaeb23aed340c1cb39cc7
Dylan Trachsel <dpportet@amazon.com>
2024-03-11 13:40:45 -0700

    feat: support edge logout

--- apps/client/src/constants/format.ts
+++ apps/client/src/constants/format.ts
@@ -4,3 +4,4 @@ export const ROOT_INDEX_PAGE_FORMAT: Format = 'default';
 export const DASHBOARDS_INDEX_PAGE_FORMAT: Format = 'table';
 export const CREATE_DASHBOARD_PAGE_FORMAT: Format = 'form';
 export const DASHBOARD_PAGE_FORMAT: Format = 'default';
+export const EDGE_LOGIN_PAGE_FORMAT: Format = 'form';
