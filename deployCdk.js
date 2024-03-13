cfc7d2c543490b7f3104fa2cb4f356425e7fc40a
Dylan Trachsel <dpportet@amazon.com>
2024-03-13 14:20:47 -0700

    feat: add edge as a deployment option

--- deployCdk.js
+++ deployCdk.js
@@ -5,6 +5,7 @@ const { execSync } = require('child_process');
 
 const cognito = 'AWS Cognito';
 const sso = 'AWS IAM Identity Center (formerly AWS SSO)';
+const edge = 'Edge';
 
 const askQuestions = () => {
     const questions = [
@@ -13,7 +14,7 @@ const askQuestions = () => {
         name: 'AUTH',
         message: 'Welcome to IoT Application. Please select an authentication mode.',
         default: cognito,
-        choices: [cognito, sso],
+        choices: [cognito, sso, edge],
       }
     ];
   
@@ -30,6 +31,16 @@ const run = async () => {
     } else if (AUTH === sso) {
       execSync("yarn workspace cdk deploy:no-review:sso", { stdio: 'inherit' });
       console.log('Please follow README instructions to complete SSO setup after deployment: https://github.com/awslabs/iot-application/tree/main/deploymentguide');
+    } else if (AUTH === edge) {
+      const edgeQuestion = [
+          {
+            type: 'input',
+            name: 'ENDPOINT',
+            message: 'What is the ip/hostname of your edge gateway? (example: htttps://1.2.3.4.5)',
+          }
+        ];
+      const { ENDPOINT } = await inquirer.prompt(edgeQuestion);
+      execSync(`yarn workspace cdk cdk deploy -c authMode=edge -c edgeEndpoint="${ENDPOINT}" --all --require-approval never`, { stdio: 'inherit' });
     } else {
       execSync("yarn workspace cdk deploy:no-review:cognito", {stdio: 'inherit' });
     }
